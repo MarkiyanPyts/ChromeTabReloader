affbe475fecb318bf2dde75d417838616ab63490
Markiyan Pyts <markiyan.pyts@osf-global.com>
2017-05-02 22:53:39 +0300

    did research on plugin communication

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var WebSocketServer = require("websocketserver");
 var server = new WebSocketServer("all", 9000);
 
 server.on('connection', function(id) {
-    console.log('hi');
+    console.log('connection open');
     server.sendMessage("one", "Welcome to the server!", id);
 });
+
+server.on("closedconnection", function(id) {
+    console.log("Connection " + id + " has left the server");
+});
+
+server.on("message", function(data, id) {
+    var mes = server.unmaskMessage(data);
+    var str = server.convertToString(mes.message);
+    console.log(str);
+});
+
